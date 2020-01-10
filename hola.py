7fa3eb8ad25e25fa073fb7c50fd7360be0227747
abeldex <abelcota@uas.edu.mx>
2020-01-10 11:40:58 -0700

    mas cambios

--- hola.py
+++ hola.py
@@ -113,10 +113,29 @@ class AllExceptionHandler(AbstractExceptionHandler):
         handler_input.response_builder.speak(speech).ask(speech)
         return handler_input.response_builder.response
 
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+
+
+
+
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(HolaMundoIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelAndStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
 sb.add_exception_handler(AllExceptionHandler())
 
 handler = sb.lambda_handler()
